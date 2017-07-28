1e5c18c775c3afca79e0d53779889d7913574d40
Hongyi Zhang <hongee.tc@gmail.com>
2017-07-28 01:09:49 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.6.2
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /kerckhoff
+
+RUN apt-get update && apt-get install -y curl \
+                                         build-essential
+RUN curl -sL https://deb.nodesource.com/setup_6.x | bash
+RUN apt-get install -y nodejs
+RUN npm install -g webpack
+
+WORKDIR /kerckhoff
+
+ADD requirements.txt /kerckhoff/
+RUN pip install -r requirements.txt
+
+ADD package.json /kerckhoff/
+RUN npm install
+
+ADD . /kerckhoff/
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  context: __dirname,
+
+  entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  output: {
+      path: path.resolve('./assets/bundles/'),
+      filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+  ],
+
+  module: {
+      rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+          }
+      ],
+  },
+}

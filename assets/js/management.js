c7438b4ee3db5e1e7d89fd3d5a557b457a106c5e
Hongyi Zhang <hongee.tc@gmail.com>
2017-09-10 22:47:44 -0700

    basic UI

--- assets/js/management.js
+++ assets/js/management.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import Icon from "vue-awesome/components/Icon";
 import "vue-awesome/icons";
+import VueUploadComponent from "vue-upload-component";
 import MainModule from "./MainModule";
 import router from "./routes/ManageRoutes";
 
@@ -10,6 +11,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+Vue.component("file-upload", VueUploadComponent);
 Vue.component("icon", Icon);
 
 const mgmt = new Vue({

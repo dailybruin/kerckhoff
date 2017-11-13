38c9d36bafda49d2b765dfb15cb73e4ed29cb44b
Rohan Khajuria <rkhajuria99@gmail.com>
2017-11-12 22:14:31 -0800

    directly convert Page queryset to json and return

--- pages/views.py
+++ pages/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import json
 from django.http import HttpResponse
-
-json_data = '{"hello": "world", "foo": "bar"}'
-data = json.loads(json_data)
+from pages.models import Page
 
 # Create your views here.
-
 
 def index(request):
     return render(request, 'index.html')
 
 def pages(request):
-    return HttpResponse(json.dumps(data), content_type='application/json')
+    return HttpResponse(json.dumps(list(Page.objects.all())), content_type='application/json')

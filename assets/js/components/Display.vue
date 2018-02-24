ca808e9c666c3df527bfe64beeeac8513d242441
Rohan Khajuria <rkhajuria99@gmail.com>
2018-02-24 15:27:36 -0800

    working on front end

--- assets/js/components/Display.vue
+++ assets/js/components/Display.vue
@@ -1,11 +1,24 @@
 <template>
 <div class="container mt-3">
   <h2>Display view!</h2>
+  {{todos}}
 </div>
 </template>
 
 <script>
 export default {
-  name: 'display-view'
+  name: 'display-view',
+  data: function () {
+    /*this.$http.get("http://localhost:5000/pages?pagesPerQuery=50&queryNumber=1", function(data) {
+      alert("hel");
+    });*/
+    return {
+      todos: [
+        { text: 'Learn Vue' },
+        { text: 'Learn Docker' },
+        { text: 'Learn Everything' }
+      ]
+    }
+  }
 }
 </script>

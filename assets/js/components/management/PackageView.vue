bc1072838dfce29ae57c0385e609fb2d74972bcb
Hongyi Zhang <hongee.tc@gmail.com>
2018-02-24 22:01:49 -0800

    WIP

---
+++ assets/js/components/management/PackageView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h2>
+        {{ $route.params.slug }}
+        <b-button class="ml-2" size="sm" variant="secondary">
+          Update from GDrive
+        </b-button>
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'package-view'
+}
+</script>

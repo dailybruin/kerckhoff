530ee29bd4bd8f4dc4441d31eaa2253b6a02b99b
Rohan Khajuria <rkhajuria99@gmail.com>
2018-03-08 23:29:43 -0800

    beginning of page view

--- assets/js/components/Display.vue
+++ assets/js/components/Display.vue
@@ -8,6 +8,13 @@
     <p>This is the future home of the Daily Bruin's online projects! <br /> The platform is <a target="_blank" href="https://github.com/daily-bruin/kerckhoff">still a work in progress</a>, but if you are here <strong>on business</strong>...</p>
     <p>You can proceed on <a href="/manage">here</a>.</p>
   </div>
+  <br />
+  <h1>WIP: Page View</h1>
+  <div>
+    <li v-for="page of pages">
+      <p>{{page.title}}</p>
+    </li>
+  </div>
 </div>
 </template>
 
@@ -25,19 +32,20 @@
 
 
 <script>
+import {axios, utils} from "../util";
+
 export default {
   name: 'display-view',
   data: function () {
-    /*this.$http.get("http://localhost:5000/pages?pagesPerQuery=50&queryNumber=1", function(data) {
-      alert("hel");
-    });*/
     return {
-      todos: [
-        { text: 'Learn Vue' },
-        { text: 'Learn Docker' },
-        { text: 'Learn Everything' }
-      ]
+      pages: []
     }
+  },
+
+  created() {
+    axios.get(`/pages?pagesPerQuery=50&queryNumber=1`).then(response => {
+      this.posts = response.data
+    })
   }
 }
 </script>

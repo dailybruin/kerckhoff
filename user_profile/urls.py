f47d4efad041bf3904eb93a74ca5a5e8456a96e7
Rohan Khajuria <rkhajuria99@gmail.com>
2018-05-08 18:45:06 -0700

    direct Google login

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
+#the URL to login through Google
+GOOGLE_LOGIN_URL = '/accounts/google/login/?process=login&next=%%2Fmanage%%2F/'
+
 urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
+    #redirects user to Google login page
+    url(r'^login/$', RedirectView.as_view(url=GOOGLE_LOGIN_URL)),
 ]

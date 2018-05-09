c65276592dafc88ee3358429db358a63ed2c01c0
katiecai <katiecai@g.ucla.edu>
2018-05-08 19:21:44 -0700

    added tags model

---
+++ tags/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Tags(models.Model):
+    value = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.value

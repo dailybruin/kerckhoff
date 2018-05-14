f6765830d8b89ec86c1f9590b01bd7ed719dd14b
Rohan Khajuria <rkhajuria99@gmail.com>
2018-05-14 15:40:20 -0700

    added feature to redirect back to original page after logging in

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import url
-from django.views.generic.base import RedirectView
 
 from . import views
-
-#the URL to login through Google
-GOOGLE_LOGIN_URL = '/accounts/google/login/?process=login&next=%%2Fmanage%%2F/'
 
 urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     #redirects user to Google login page
-    url(r'^login/$', RedirectView.as_view(url=GOOGLE_LOGIN_URL)),
+    url(r'^login/$', views.redirectToGoogle, name="rediect_to_Google"),
 ]

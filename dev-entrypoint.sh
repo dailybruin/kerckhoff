7b6aa5b198bf15fcddf61b56fd1c8d93e3e49fab
yjh020711@ucla.edu <yjh020711@ucla.edu>
2023-02-09 20:22:47 -0800

    init script

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 rsync -arv --progress /usr/src/cache/node_modules/. /kerckhoff/node_modules/
 cmd="nohup npm run watch";
 $cmd &
+python3.7 kerckhoff/manage.py makemigrations
 python3.7 kerckhoff/manage.py migrate
 exec "$@"

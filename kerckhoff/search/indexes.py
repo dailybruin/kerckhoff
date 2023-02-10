a3ee54ea0624dfb3c57aa50ee0f86a24c9933bae
yjh020711@ucla.edu <yjh020711@ucla.edu>
2023-02-09 21:02:54 -0800

    update elasticsearch syntax and archieml dependency

--- kerckhoff/search/indexes.py
+++ kerckhoff/search/indexes.py
@@ -1,8 +1,8 @@
 from elasticsearch_dsl import Document, Text, Keyword, Date
 
 class PackageIndex(Document):
-    class Meta:
-        index = 'package-index'
+    class Index:
+        name = 'package-index'
     slug = Keyword()
     package_set = Keyword()
     description = Text()
